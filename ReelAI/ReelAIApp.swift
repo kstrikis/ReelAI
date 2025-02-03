cfb285e5c5f56722836bfd90a88df02e141c91c9
Kriss Strikis <kstrikis@gmail.com>
2025-02-03 12:10:28 -0600

    Initial Commit

---
+++ ReelAI/ReelAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelAIApp.swift
+//  ReelAI
+//
+//  Created by Kriss on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ReelAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
